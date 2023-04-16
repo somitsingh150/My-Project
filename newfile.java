a54071f4e04eba0556f5f23b1f50843e0027f2f9
somitsingh150 <130898402+somitsingh150@users.noreply.github.com>
2023-04-16 12:21:10 +0300

    Add files via upload

---
+++ newfile.java
@@ -0,0 +1,14 @@
+
+public class newfile {
+
+ static int a=2;
+ static int i ;
+    public static void main(String args[]) {
+        printTable ();
+    }
+    static void printTable (){
+        for (i=1;i<=20;i++){
+            System.out.println(a+"*"+i + " = "+a*i);
+        }
+    }
+}
